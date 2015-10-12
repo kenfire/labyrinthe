8a230346c897036ecd3acc4bb49e3d3af14dec42
kenfire <root@localhost.localdomain>
2015-10-12 23:45:05 +0200

    Begun creation of labyrinth

--- affichage.c
+++ affichage.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <time.h>
 
-int random(int min, int max)
+int random (int min, int max)
 {
     srand(time(NULL));
     return (int) rand()%max + min;
 }
-
-void create_labyrinthe(int max_line, int max_col, int arr[max_line][max_col])
+/*
+ * 
+*/
+void create_grid (int max_line, int max_col, int arr[max_line][max_col])
 {
     int col, line;
     int cell_num=1;
@@ -25,7 +27,36 @@ void create_labyrinthe(int max_line, int max_col, int arr[max_line][max_col])
     }
 }
 
-void print_labyrinthe(int max_line, int max_col)
+void create_labyrinthe (int max_line, int max_col, int arr[max_line][max_col])
+{
+    int finish = 0;
+    int alea_line = 0;
+    int alea_col = 0;
+    int min_cell = 1;
+    int max_cell = (max_line -1) * (max_col - 1) / 4;
+
+    create_grid(max_line, max_col, arr);
+    
+    while (!finish)
+    {
+        alea_line = random(min_cell + 2, max_cell - 2);
+        alea_col = random(min_cell + 2, max_cell - 2);
+        
+        if (alea_line %2 != 0)
+        { 
+            alea_line += 1;
+        }
+
+        if (alea_col %2 != 0)
+        {
+            alea_col += 1;
+        }
+
+        printf("%d -- %d\n", alea_line, alea_col);
+        finish = 1;
+    }
+}
+void print_labyrinthe (int max_line, int max_col)
 {
     int col, line, rand_num;
     int cell_num=1;
